4b723e1d2199902c5595592ed40940f229ec7855
Kris Wawrzyniak <kwawrzyniak@ufl.edu>
2019-02-21 14:15:08 -0500

    updated schema

--- src/resources/students/schema.js
+++ src/resources/students/schema.js
@@ -2,18 +2,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var studentsSchema = new Schema({
+	id: String,
   name: String,
 	major: String,
+	school: String,
+	graduation_semester: String,
+	emails: {
+		UFL: String,
+		permanent: String
+	},
+	phone: String,
 	personal_statement: String,
-	headshot: String,
 	pillar: {
 		semester_joined: String,
 		semester_left: String,
-		title: String
+		pillar_name: String
 	},
-  positions: [String],
-  email: [String],
-	phone: String,
+  positions: {
+		current: String,
+		past: [String]
+	},
 	profile_links: {
 		linkedin_url: String,
 		twitter: String,
@@ -22,16 +30,38 @@ var studentsSchema = new Schema({
 		personal_website: String,
 		facebook: String
 	},
-  isAlumni: Boolean,
+	isAlumni: Boolean,
+	isGraduating: Boolean,
 	isHired: Boolean,
   current_employment: {
 		employer: String,
 		job_title: String
 	},
-  isPlaced: Boolean,
-	PlacedAt: String,
-	skills: [String],
-	project_assignments: [String]
+	project_assignments: {
+		first: String,
+		second: String,
+		third: String
+	},
+	birthdate: String,
+	emergency_contact1: {
+		contact1_name: String,
+		contact1_phone: String,
+		contact1_alt_phone: String
+	},
+	emergency_contact2: {
+		contact2_name: String,
+		contact2_phone: String,
+		contact2_alt_phone: String
+	},
+	allergies: [String],
+	race: {
+		first: String,
+		second: String
+	},
+	ethnicity: {
+		first: String,
+		second: String
+	}
 });
 
 var Student = mongoose.model('students', studentsSchema,'students');
